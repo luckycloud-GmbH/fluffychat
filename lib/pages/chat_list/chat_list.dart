0af56d5d72e2f68ad98059669aef0498a085960c
Viktor Rieger <viktor.rieger@luckycloud.de>
2024-11-16 03:05:12 +0100

    fix loading screen bug in the chat list page

--- lib/pages/chat_list/chat_list.dart
+++ lib/pages/chat_list/chat_list.dart
@@ -836,6 +836,8 @@ class ChatListController extends State<ChatList>
           await BootstrapDialog(client: client).show(context);
         }
       }
+      await client.oneShotSync();
+      Logs().i('Updated preBatch: ${client.prevBatch}');
     }
     if (!mounted) return;
     setState(() {
